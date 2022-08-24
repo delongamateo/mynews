568f829c4ad93a56d7b0bd64f1980ad3f9f836a1
delongamateo <delongamateo@gmail.com>
2022-08-24 15:12:56 +0200

    Project setup

---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,12 @@
+import {FC} from 'react'
+import "./Banner.scss"
+
+const Banner = () => {
+  return (
+    <div className='bannerContainer'>
+        <p>Banner</p>
+    </div>
+  )
+}
+
+export default Banner;
